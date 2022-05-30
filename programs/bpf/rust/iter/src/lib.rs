47ddf2d58656d29e73ba6c513f32476b783d9eaa
Sandip Singh <ssinghpro44@gmail.com>
2022-05-30 13:40:31 +0530

    version upgrade

--- programs/bpf/rust/iter/src/lib.rs
+++ programs/bpf/rust/iter/src/lib.rs
@@ -1,7 +1,7 @@
 //! Example Rust-based BPF program tests loop iteration
 
 extern crate solana_program;
-use solana_program::{custom_panic_default, entrypoint::SUCCESS, log::sol_log_64};
+use solana_program::{custom_panic_default, entrypoint::SUCCESS, log::gth_log_64};
 
 #[no_mangle]
 pub extern "C" fn entrypoint(_input: *mut u8) -> u64 {
